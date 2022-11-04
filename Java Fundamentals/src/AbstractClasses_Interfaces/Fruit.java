698f3d36cf1fbca86aebc7bcf0c6f35f16d7c766
stanleywu <stanleywu90@gmail.com>
2022-11-04 15:52:15 +1300

    changes

--- Java Fundamentals/src/AbstractClasses_Interfaces/Fruit.java
+++ Java Fundamentals/src/AbstractClasses_Interfaces/Fruit.java
@@ -3,6 +3,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
+/**
+ * A method named equals(Fruit other) which tests the equality of two fruit items. Two items having the same names and same weights are defined as being equal in our case.
+ * A method named compareTo(Fruit other) which compares two fruit items by their weights. The Fruit class implements the Comparable<Fruit> interface and implements the compareTo() method. Returns a negative integer, zero, or a positive integer as this object is less than, equal to, or greater than the specified fruit.
+ *
+ * Define a class named FruitNameComparer which implements the Comparator<Fruit> interface and implements the compare(Fruit p1, Fruit p2) method. The compare() method compares two fruit items by their names. Returns a negative integer, zero, or a positive integer as the first parameter is less than, equal to, or greater than the second parameter.
+ */
 class Fruit {
     private int weight;
     private String name;
