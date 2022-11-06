d8c749cb9acf81e4dd3e765d203efe6b1e6b9015
stanleywu <stanleywu90@gmail.com>
2022-11-06 19:51:22 +1300

    changes

--- Java Fundamentals/src/Nested_Exceptions_FileIO/ProcessMarks.java
+++ Java Fundamentals/src/Nested_Exceptions_FileIO/ProcessMarks.java
@@ -5,7 +5,26 @@ import java.io.IOException;
 import java.util.Scanner;
 
 /**
+ *Define a static method named processMarks(String filename) which is passed two parameters:
  *
+ * filename - the name of a text file containing a list of marks (one mark per student) which is to be read into the program,
+ * course_name - the name of the course.
+ * The input file contains a list of marks (whole numbers), one mark per line. The function reads the list of marks from the input file, processes the marks by calculating the total number of marks (the number of students in the class) and the average mark, and finally, prints a summary. For example, if the file, "Marks.txt" contains the following numbers:
+ *
+ * 79
+ * 98
+ * 50
+ * 21
+ * 48
+ * 100
+ * The following code:
+ *
+ * process_marks("Marks.txt", "CompSci 123")
+ * produces the file with the following result:
+ *
+ * Compsci 123
+ * Number of students: 6
+ * Average mark: 66
  */
 
 public class ProcessMarks {
