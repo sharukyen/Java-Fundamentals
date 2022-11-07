08cc2436fdded3e522a16887555cbbe36e798215
stanleywu <stanleywu90@gmail.com>
2022-11-07 15:33:32 +1300

    changes

--- Java Fundamentals/src/Arrays_ArrayLists/RemovePositives.java
+++ Java Fundamentals/src/Arrays_ArrayLists/RemovePositives.java
@@ -2,7 +2,15 @@ package Arrays_ArrayLists;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-
+/**
+ *Define a static method named removePositives(ArrayList<Integer> numbers) which takes an array list of integers as a parameter and removes all positive integers from the parameter array list.
+ *
+ * Note:
+ *
+ * The method makes changes in place. (i.e. the method updates the parameter array list and it does not return a new array list).
+ * You can assume that the array list is not empty.
+ * Hint: you will need to remove numbers from the end of the array list.
+ */
 public class RemovePositives {
     public static void removePositives(ArrayList<Integer> numbers) {
         for (int i = numbers.size() - 1; i >= 0; i--) {
