26ed31489925afc969ccd309876959812b93fbb5
stanleywu <stanleywu90@gmail.com>
2022-11-07 15:30:12 +1300

    changes

--- Java Fundamentals/src/Arrays_ArrayLists/ReversePoints.java
+++ Java Fundamentals/src/Arrays_ArrayLists/ReversePoints.java
@@ -2,7 +2,11 @@ package Arrays_ArrayLists;
 
 import java.awt.*;
 import java.util.ArrayList;
-
+/**
+ *Write a static method named reverse_points(ArrayList<Point> points) that takes an array list of Point objects as a parameter and reverses the x-coordinate and y-coordinate of each element in the parameter array list.
+ *
+ * Note: the array list is changed in-place. (i.e., the method updates the parameter array list and it doe not return a new array list). You can assume that the parameter list is not empty.
+ */
 public class ReversePoints {
     public static void reverse_points(ArrayList<Point> points) {
         for (Point p : points) {
