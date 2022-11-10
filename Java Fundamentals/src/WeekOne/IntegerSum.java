8c966594e85a7bd06217f1df91692e7feae8f416
stanleywu <stanleywu90@gmail.com>
2022-11-10 18:12:16 +1300

    classes

---
+++ Java Fundamentals/src/WeekOne/IntegerSum.java
@@ -0,0 +1,21 @@
+package WeekOne;
+
+import java.util.Scanner;
+
+/**
+ *
+ */
+
+public class IntegerSum {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num1 = input.nextInt();
+        System.out.println("Enter another number:");
+        int num2 = input.nextInt();
+        int sum = num1 + num2;
+        System.out.printf("The sum is %d.", sum);
+
+    }
+}
