e91d32599f4ea8189226aaa36e1a663828e8e0fc
stanleywu <stanleywu90@gmail.com>
2022-11-10 18:12:54 +1300

    classes

---
+++ Java Fundamentals/src/WeekOne/TypeConverter.java
@@ -0,0 +1,18 @@
+package WeekOne;
+
+/**
+ *converts an int to a String and converts from a String to an int.
+ */
+
+public class TypeConverter {
+
+    public static void main(String[] args) {
+        int value = 12;
+        String myString = "" + 12;
+        System.out.println(myString);
+
+        String myValue = "123";
+        int myInt = 123;
+        System.out.println(myInt);
+    }
+}
