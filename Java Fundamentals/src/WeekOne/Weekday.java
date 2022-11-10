76f53da5802971f2247f403308b70711558184de
stanleywu <stanleywu90@gmail.com>
2022-11-10 18:13:47 +1300

    classes

---
+++ Java Fundamentals/src/WeekOne/Weekday.java
@@ -0,0 +1,15 @@
+package WeekOne;
+
+public enum Weekday {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+}
+class weekTest{
+    public static void main(String args[]) {
+        Weekday a_day = Weekday.FRIDAY;
+        System.out.println(a_day);
+    }
+}
